135865eeff3de6fca3f710e72dc678577e63bca8
Sofia Porto <racewithsofia@gmail.com>
2026-05-31 12:29:44 -0300

    js

--- script.js
+++ script.js
@@ -28,21 +28,29 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagem)
     botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagem(evento) {
-    console.log(evento.target.value)
 function receberPorcentagemBotao(evento) {
+function receberPorcentagem(evento) {
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
 
-        if(botao.value === evento.target.value) {
-            botao.classList.add("botao-ativo")
+function receberPorcentagemBotao(evento) {
         }
     })
 
     porcentagem = parseFloat(evento.target.value) / 100
+    if(evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value) / 100
+    } else {
+        porcentagem = 0
+    }
+   
     console.log(porcentagem)
 }
+}
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
